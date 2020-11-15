e606500ffcfcf2716c5707b7e5ab1c2c072d6d01
sandilokaa <sandyloka790@students.unnes.ac.id>
2020-11-15 23:07:39 +0700

    Add <loka> Modif admin

--- admin/js/demo/chart-area-demo.js
+++ admin/js/demo/chart-area-demo.js
@@ -32,21 +32,21 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Jan",  "Mar",  "May",  "Jul", "Sep",  "Nov", "Dec" ],
     datasets: [{
-      label: "Earnings",
+      label: "Visitor ",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(78, 115, 223, 1)",
+      borderColor: "#15A54E",
       pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointBackgroundColor: "#15A54E",
+      pointBorderColor: "#15A54E",
       pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverBackgroundColor: "#84E5AB",
+      pointHoverBorderColor: "#84E5AB",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0,  2000, 6000,  9000, 12000,  14200, 15666],
     }],
   },
   options: {
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return  number_format(value);
           }
         },
         gridLines: {
@@ -110,7 +110,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + number_format(tooltipItem.yLabel);
         }
       }
     }
--- admin/js/demo/chart-pie-demo.js
+++ admin/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Member", "Like", "Share"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [666, 420, 230],
+      backgroundColor: ['#FFAE12', '#FF1212', '#027CEC'],
+      hoverBackgroundColor: ['#FFCC69', '#F95B5B', '#64B5FF'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
